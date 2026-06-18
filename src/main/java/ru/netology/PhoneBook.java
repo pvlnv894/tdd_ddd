f11f71a42aed3240a6cbeef93e6664d3c4051eae
Pavel Novichkov <novickovpavel3@gmail.com>
2026-06-18 19:30:31 +0700

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,12 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> phoneBook = new HashMap<>();
+
+    public long add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    void shouldReturnCorrectCountAfterAddingElement() {
+        PhoneBook phoneBook = new PhoneBook();
+
+        assertEquals(1, phoneBook.add("Ivan", "1"));
+        assertEquals(2, phoneBook.add("Petr", "2"));
+        assertEquals(3, phoneBook.add("Sasha", "3"));
+    }
 }
