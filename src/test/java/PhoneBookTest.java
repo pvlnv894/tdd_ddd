6dcabeb17e705041ec69cf82cff1821eeefb7287
Pavel Novichkov <novickovpavel3@gmail.com>
2026-06-18 19:45:01 +0700

    Stub and tests for printAllNames

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 import ru.netology.PhoneBook;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PhoneBookTest {
@@ -30,5 +32,19 @@ public class PhoneBookTest {
         phoneBook.add("Ivan", "1");
 
         assertEquals("1", phoneBook.findByName("Ivan"));
+    }
+
+    @Test
+    void shouldReturnNamesInAlphabeticalOrder() {
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add("Ivan", "1");
+        phoneBook.add("Petr", "2");
+        phoneBook.add("Anna", "3");
+
+        List<String> actual = phoneBook.printAllNames();
+        List<String> expected = List.of("Anna", "Ivan", "Petr");
+
+        assertEquals(expected, actual);
     }
 }
